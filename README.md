589ea220d7e7d900d18c799deded144bdd400a28
WizzyGeek <ojasscoding@gmail.com>
2024-08-22 00:50:31 +0530

    misc: Deployment notes

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # ComAPI
 
 A simple Asp.NET MVC based API to extract free text comments from multiple pdf files
+
+## Deployment Notes
+
+1. Change the Allowed origins in the CORS policy
+2. Give it ample amout of RAM
--- src/Program.cs
+++ src/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.Net.Http.Headers;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,6 +9,19 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+var AllowAll = "AllowAll";
+const string Origins = "*";
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: AllowAll,
+        policy  =>
+        {
+            policy.WithOrigins(Origins)
+            .WithHeaders(HeaderNames.ContentDisposition);
+        });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -15,6 +30,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors(AllowAll);
 
 app.UseHttpsRedirection();
 
